4db824208b163cc5483a7a652047851488a50708
Christopher L. Crutchfield <ccrutchf@ucsd.edu>
2024-05-01 10:02:31 -0700

    feat: rust/swift integration.  Use pinch to zoom to trigger demo dialog box

---
+++ rust-bridge/src/lib.rs
@@ -0,0 +1,17 @@
+use fishsense::add;
+
+#[no_mangle]
+pub extern fn add2(left: usize, right: usize) -> usize {
+    add(left, right)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add2(2, 2);
+        assert_eq!(result, 4);
+    }
+}
