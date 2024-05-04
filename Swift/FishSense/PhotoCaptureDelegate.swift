8d5bcdc8ac8f8f4187374f8fdc6dcde08323f134
Harish Vasanth <havasanth@Rushils-MacBook-Pro.local>
2024-05-03 19:38:41 -0700

    potentially getting distance calculation to work?

--- Swift/FishSense/PhotoCaptureDelegate.swift
+++ Swift/FishSense/PhotoCaptureDelegate.swift
@@ -56,11 +56,12 @@ extension PhotoCaptureProcessor: AVCapturePhotoCaptureDelegate {
                     let creationRequest = PHAssetCreationRequest.forAsset()
                     options.uniformTypeIdentifier = self.requestedPhotoSettings.processedFileType.map { $0.rawValue }
                     
-                    var resourceType = PHAssetResourceType.photo
+                    let resourceType = PHAssetResourceType.photo
                     creationRequest.addResource(with: resourceType, data: photoData!, options: options)
 
                     // Specify the location in which the photo was taken.
                     creationRequest.location = self.location
+                    print(self.getPoints(avDepthData: photo.depthData!, point1: CGPoint(x: 0, y: 0), point2: CGPoint(x: 3023, y: 0)))
                     
                 }, completionHandler: { _, error in
                     if let error = error {
@@ -75,5 +76,41 @@ extension PhotoCaptureProcessor: AVCapturePhotoCaptureDelegate {
                 self.didFinish()
             }
         }
+    }
+    
+    private func getPoints(avDepthData: AVDepthData, point1: CGPoint, point2: CGPoint) -> Float {
+        let depthData = avDepthData.converting(toDepthDataType: kCVPixelFormatType_DepthFloat32)
+        let intrinsicMatrix = avDepthData.cameraCalibrationData?.intrinsicMatrix
+        let depthDataMap = depthData.depthDataMap
+        CVPixelBufferLockBaseAddress(depthDataMap, CVPixelBufferLockFlags(rawValue: 0))
+           
+        let width = CVPixelBufferGetWidth(depthDataMap)
+        let height = CVPixelBufferGetHeight(depthDataMap)
+           
+        let focalX = Float(width) * ((intrinsicMatrix?[0][0] ?? 0.0) / 3024)
+        let focalY = Float(height) * ((intrinsicMatrix?[1][1] ?? 0.0) / 4032)
+        let principalPointX = Float(width) * ((intrinsicMatrix?[2][0] ?? 0.0) / 3024)
+        let principalPointY = Float(height) * ((intrinsicMatrix?[2][1] ?? 0.0) / 4032)
+
+        let floatBuffer = unsafeBitCast(CVPixelBufferGetBaseAddress(depthDataMap), to: UnsafeMutablePointer<Float32>.self)
+        
+        let x1 = Float(point1.x) / (3024/Float(width))
+        let y1 = Float(point1.y) / (4032/Float(height))
+        let x2 = Float(point2.x) / (3024/Float(width))
+        let y2 = Float(point2.y) / (4032/Float(height))
+        
+        let Z1 = floatBuffer[Int(x1 * y1)]
+        let Z2 = floatBuffer[Int(x2 * y2)]
+        
+        let X1 = (x1 - principalPointX) * Z1 / focalX
+        let Y1 = (y1 - principalPointY) * Z1 / focalY
+        
+        let X2 = (x2 - principalPointX) * Z2 / focalX
+        let Y2 = (y2 - principalPointY) * Z2 / focalY
+        
+        CVPixelBufferUnlockBaseAddress(depthDataMap, CVPixelBufferLockFlags(rawValue: 0))
+        
+        return sqrt(pow(X1 - X2, 2) + pow(Y1 - Y2,2) + pow(Z1 - Z2,2))
+
     }
 }
